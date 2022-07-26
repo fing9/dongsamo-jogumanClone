851d75dda90f0d076d4abd72ce9357e0b502f109
Eunice991217 <org9899@naver.com>
2022-07-27 02:15:05 +0900

    main commit

---
+++ src/main/resources/templates/script.js
@@ -0,0 +1,47 @@
+// $(document).ready(function(){
+//     listing();
+// });
+// //로딩후 바로실행
+// function listing() {
+//     $('#subImg').empty() //먼저 만들어두었던 이미지 카드 제거
+//     $.ajax({
+//         type:"GET",
+//         // url="", //img api
+//         data:{},
+//         success:function (response) {
+//             let rows=response['images'] //img name
+//             for(let i=0;i<rows.length;i++) {
+//                 let image=rows[i]['image']
+//
+//                 let temp_html=`<div class="col">
+//                                     <div class="card">
+//                                         <img src="${image}" class="card-img-top" alt="...">
+//                                     </div>
+//                                 </div>`
+//
+//                 $('#subImg').append(temp_html) //이미지 카드 붙이기
+//             }
+//         }
+//     })
+// }
+
+//mainImg 화면 전환 script
+var index = 0;   //이미지에 접근하는 인덱스
+window.onload = function(){
+    slideShow();
+}
+
+function slideShow() {
+    var i;
+    var x = document.getElementsByClassName("slide1");  //slide1에 대한 dom 참조
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";   //처음에 전부 display를 none으로 지정 (안보이게)
+    }
+    index++;
+    if (index > x.length) {
+        index = 1;  //인덱스가 초과되면 1로 변경
+    }
+    x[index-1].style.display = "block";  //해당 인덱스는 block으로 (보이게)
+    setTimeout(slideShow, 3000);   //함수를 3초마다 호출
+
+}
