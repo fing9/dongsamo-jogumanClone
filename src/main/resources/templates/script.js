a0f12e2577e293eeb1d833cd02766704de60b163
GS_song <20003204@sju.ac.kr>
2022-08-05 21:37:26 +0900

    make login page, admin pagination

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -25,26 +25,26 @@
 //     })
 // }
 
-//mainImg 화면 전환 script
-var index = 0;   //이미지에 접근하는 인덱스
-window.onload = function(){
-    slideShow();
-}
-
-function slideShow() {
-    var i;
-    var x = document.getElementsByClassName("slide1");//slide1에 대한 dom 참조
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";   //처음에 전부 display를 none으로 지정 (안보이게)
-    }
-    index++;
-    if (index > x.length) {
-        index = 1;  //인덱스가 초과되면 1로 변경
-    }
-    x[index-1].style.display = "block";  //해당 인덱스는 block으로 (보이게)
-    setTimeout(slideShow, 3000);   //함수를 3초마다 호출
-
-}
+// //mainImg 화면 전환 script
+// var index = 0;   //이미지에 접근하는 인덱스
+// window.onload = function(){
+//     slideShow();
+// };
+//
+// function slideShow() {
+//     var i;
+//     var x = document.getElementsByClassName("slide1");//slide1에 대한 dom 참조
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";   //처음에 전부 display를 none으로 지정 (안보이게)
+//     }
+//     index++;
+//     if (index > x.length) {
+//         index = 1;  //인덱스가 초과되면 1로 변경
+//     }
+//     x[index-1].style.display = "block";  //해당 인덱스는 block으로 (보이게)
+//     setTimeout(slideShow, 3000);   //함수를 3초마다 호출
+//
+// }
 
 //Header 불러오는 코드
 window.addEventListener('load', function() {
@@ -63,38 +63,42 @@ window.addEventListener('load', function() {
         }
     });
 });
-//siema slider 코드
-function onInitCallback() {
-    console.log('Siema initialised bro :)');
-}
+//siema slider 코드 (Jquery 강의 38)
+$(function(){
+    function onInitCallback() {
+        console.log('Siema initialised bro :)');
+    }
+    function onChangeCallback() {
+        console.log(`The index of current slide is: ${this.currentSlide}`);
+    }
+    var timer
 
-function onChangeCallback() {
-    console.log(`The index of current slide is: ${this.currentSlide}`);
-}
-var timer
-const mySiema = new Siema({
-    onInit: onInitCallback,
-    onChange: onChangeCallback,
-    loop: true,
-});
-function autoSlide(){
-    timer = setInterval(function(){
-        mySiema.next();
-    }, 4000);
-}
-autoSlide();
-$('.Slider').mouseover(function(){
-    clearInterval(timer);
-}).mouseout(function(){
+    const mySiema = new Siema({
+        selector:'.slider',
+        onInit: onInitCallback,
+        onChange: onChangeCallback,
+        loop: true,
+    });
+    function autoSlide(){
+        timer = setInterval(function(){
+            mySiema.next();
+        }, 4000);
+    }
     autoSlide();
+    $('.slider').mouseover(function(){
+        clearInterval(timer);
+    }).mouseout(function(){
+        autoSlide();
+    });
+
+    $('.prev').click(function(){
+        mySiema.prev();
+    });
+    $('.next').click(function(){
+        mySiema.next();
+    });
+
 });
-
-
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-
-prev.addEventListener('click', () => mySiema.prev());
-next.addEventListener('click', () => mySiema.next());
 
 //pagination 코드 (Jquery 강의 69)
 $(function(){
@@ -107,21 +111,24 @@ $(function(){
     /* 페이지네이션 li생성 반복문*/
     for(var i = 1; i<=pageCount; i++) {
         numbers.append('<li><a href="#">' + i +'</a></li>');
-    }
-    numbers.find('li:first-chile a').addClass('active');
+    };
+    numbers.find('li:first-child a').addClass('active');
 
     //페이지네이션 함수
     function displayRows(idx){
-        var start = (idx -1) * rowsPerPage,
+        var start = (idx - 1) * rowsPerPage,
             end = start + rowsPerPage;
 
             rows.hide();
-            rows.slide(start, end).show();
-    }
+            rows.slice(start, end).show();
+    };
+    displayRows(1);
 
-    numbers.find('li').click(function(e){
+    numbers.find('li a').click(function(e){
         e.preventDefault();
-        var index = $(this).index() + 1;
+        numbers.find('li a').removeClass('active');
+        $(this).addClass('active');
+        var index = $(this).text();
         displayRows(index);
-    })
-})
+    });
+});
