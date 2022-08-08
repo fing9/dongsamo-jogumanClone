f155a99664b6c2aabc4076c844858f8941221213
GS_song <20003204@sju.ac.kr>
2022-08-08 13:18:03 +0900

    thymeleaf code

--- src/main/resources/templates/script.js
+++ src/main/resources/templates/script.js
@@ -25,26 +25,26 @@
 //     })
 // }
 
-// //mainImg 화면 전환 script
-// var index = 0;   //이미지에 접근하는 인덱스
-// window.onload = function(){
-//     slideShow();
-// };
-//
-// function slideShow() {
-//     var i;
-//     var x = document.getElementsByClassName("slide1");//slide1에 대한 dom 참조
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";   //처음에 전부 display를 none으로 지정 (안보이게)
-//     }
-//     index++;
-//     if (index > x.length) {
-//         index = 1;  //인덱스가 초과되면 1로 변경
-//     }
-//     x[index-1].style.display = "block";  //해당 인덱스는 block으로 (보이게)
-//     setTimeout(slideShow, 3000);   //함수를 3초마다 호출
-//
-// }
+//mainImg 화면 전환 script
+var index = 0;   //이미지에 접근하는 인덱스
+window.onload = function(){
+    slideShow();
+};
+
+function slideShow() {
+    var i;
+    var x = document.getElementsByClassName("slide1");//slide1에 대한 dom 참조
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";   //처음에 전부 display를 none으로 지정 (안보이게)
+    }
+    index++;
+    if (index > x.length) {
+        index = 1;  //인덱스가 초과되면 1로 변경
+    }
+    x[index-1].style.display = "block";  //해당 인덱스는 block으로 (보이게)
+    setTimeout(slideShow, 3000);   //함수를 3초마다 호출
+
+}
 
 //Header 불러오는 코드
 window.addEventListener('load', function() {
@@ -78,6 +78,7 @@ $(function(){
         onInit: onInitCallback,
         onChange: onChangeCallback,
         loop: true,
+        duration: 800,
     });
     function autoSlide(){
         timer = setInterval(function(){
