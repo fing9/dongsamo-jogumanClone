1e7eca5a2e920d9561ea2b10eec81e08c3d9d646
GS_song <20003204@sju.ac.kr>
2022-08-13 02:22:11 +0900

    add myPage userInfoFix, Secession modal and add order function

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,24 +1,24 @@
-//mainImg 화면 전환 script
-var index = 0;   //이미지에 접근하는 인덱스
-window.onload = function () {
-    slideShow();
-}
-
-function slideShow() {
-    var i;
-    var x = document.getElementsByClassName("slide1");  //slide1에 대한 dom 참조
-
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";   //처음에 전부 display를 none으로 지정 (안보이게)
-    }
-    index++;
-    if (index > x.length) {
-        index = 1;  //인덱스가 초과되면 1로 변경
-    }
-    x[index - 1].style.display = "block";  //해당 인덱스는 block으로 (보이게)
-    setTimeout(slideShow, 3000);   //함수를 3초마다 호출
-
-}
+// //mainImg 화면 전환 script
+// var index = 0;   //이미지에 접근하는 인덱스
+// window.onload = function () {
+//     slideShow();
+// }
+//
+// function slideShow() {
+//     var i;
+//     var x = document.getElementsByClassName("slide1");  //slide1에 대한 dom 참조
+//
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";   //처음에 전부 display를 none으로 지정 (안보이게)
+//     }
+//     index++;
+//     if (index > x.length) {
+//         index = 1;  //인덱스가 초과되면 1로 변경
+//     }
+//     x[index - 1].style.display = "block";  //해당 인덱스는 block으로 (보이게)
+//     setTimeout(slideShow, 3000);   //함수를 3초마다 호출
+//
+// }
 
 
 //pagination 코드 (Jquery 강의 69)
@@ -127,59 +127,53 @@ $(function(){
         day.append('<option value="i">'+i+'</option>');
     }
 });
-//order 주문자 정보 변경 버튼
-$(function(){
-    var fixBtn = $('#orderUserFixBtn');
-
-
-    fixBtn.click(function (){
-        $('.orderUserInfoDefault').css({
-            display: 'none'
-        });
-        $('.orderUserInfoChange').css({
-            display: 'block'
-        });
-        fixBtn.css({
-            display: 'none'
-        });
-    });
-
-});
-//order 배송정보 변경 버튼
+//order 주문자,배송정보 변경 버튼
 $(function (){
     var changeBtn = $('#orderDeliveryChange');
+    var fixBtn = $('#orderUserFixBtn');
 
+    fixBtn.click(function (){
+        $('.orderUserInfoDefault').css({display: 'none'});
+        $('.orderUserInfoChange').css({});
+        fixBtn.css({display: 'none'});
+    });
     changeBtn.click(function(){
-        $('.orderProductDeliveryDefault').css({
-            display: 'none'
-        });
-        $('.orderProductDeliveryChange').css({
-            display: 'block'
-        });
-        changeBtn.css({
-            display: 'none'
-        });
+        $('.orderProductDeliveryDefault').css({display: 'none'});
+        $('.orderProductDeliveryChange').css({display: 'block'});
+        changeBtn.css({display: 'none'});
     });
     $('.orderSelectBtn').click(function(){
-        $('.orderProductDeliveryDefault').css({
-            display: 'none'
-        });
-        $('.orderProductDeliveryTab').css({
-            display: 'block'
-        });
-        $('.orderProductDeliveryTabNew').css({
-            display: 'none'
-        });
+        $('.orderProductDeliveryDefault').css({display: 'none'});
+        $('.orderProductDeliveryTab').css({display: 'block'});
+        $('.orderProductDeliveryTabNew').css({display: 'none'});
     })
     $('.orderNewBtn').click(function (){
-        $('.orderProductDeliveryDefault').css({
-            display: 'none'
-        });
-        $('.orderProductDeliveryTab').css({
-            display: 'none'
-        });
-        $('.orderProductDeliveryTabNew').css({
-            display: 'block'
-        });
+        $('.orderProductDeliveryDefault').css({display: 'none'});
+        $('.orderProductDeliveryTab').css({display: 'none'});
+        $('.orderProductDeliveryTabNew').css({display: 'block'});
     })
+});
+//order 총금액 계산
+$(function (){
+    var productFee = $('.orderProductFee p:nth-child(2)').text().slice(1);
+    var delivaryFee = $('.orderProductDelFee p:nth-child(2)').text().slice(2);
+
+    function addComma(value){
+        value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return value;
+    }
+    function minusComma(value){
+        value = value.replace(/[^\d]+/g, "");
+        return value;
+    }
+
+    productFee = minusComma(productFee);
+    delivaryFee = minusComma(delivaryFee);
+
+    var totalFee = parseInt(productFee) + parseInt(delivaryFee);
+    totalFee = String(totalFee);
+    totalFee = addComma(totalFee);
+
+    $('.orderSummaryTotal').append('<p>₩' + totalFee +'</p>');
+
 });
