7e15110f1726a7ee7836c4db00e3a53cff3b1fca
GS_song <20003204@sju.ac.kr>
2022-08-15 17:56:44 +0900

    bug fix main page

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,24 +1,3 @@
-//mainImg 화면 전환 script
-let index = 0;   //이미지에 접근하는 인덱스
-window.onload = function(){
-    slideShow();
-}
-
-function slideShow() {
-    let i;
-    let x = document.getElementsByClassName("slide1");  //slide1에 대한 dom 참조
-
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";   //처음에 전부 display를 none으로 지정 (안보이게)
-    }
-    index++;
-    if (index > x.length) {
-        index = 1;  //인덱스가 초과되면 1로 변경
-    }
-    x[index-1].style.display = "block";  //해당 인덱스는 block으로 (보이게)
-    setTimeout(slideShow, 3000);   //함수를 3초마다 호출
-}
-
 //위시리스트
 function heartClickOn(e) {
     const normal=document.querySelector("#normalHeart");
